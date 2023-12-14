cbdecd96ffd41856fdea34e1addb01994b0bdc84
patrickpetty23 <pettyp@icloud.com>
2023-12-14 14:28:59 -0700

    adding crud stuff

--- index.js
+++ index.js
@@ -42,65 +42,49 @@ function checkLoggedIn (req, res, next) {
     }  
 }
 
-// route to display create.ejs
-// app.get("/create", checkLoggedIn, (req, res) => {
-//     let errorMessage = null;
-//     let successMessage = null;
-//     let loggedInUsername = req.session.username;
-
-//     if(loggedInUsername === "admin"){
-//         res.render("create", {errorMessage, successMessage, loggedIn: req.session.loggedIn});
-//     }
-//     else{
-//         res.render("login", {loggedIn: req.session.loggedIn});
-//     }
-// });
-
-// app.post('/create', async (req, res) => {
-//     const { username, password, confirmPassword } = req.body;
-//     let errorMessage = null;
-
-//     // Check if the username already exists
-//     const dbUser = await knex("logins").select().where('username', '=', username);
-
-//     if (dbUser.length > 0) {
-//         // Username already exists
-//         errorMessage = 'That username is already being used';
-//     } else {
-//         // Continue with the user creation logic
-//         if (password !== confirmPassword) {
-//             errorMessage = 'Passwords need to match';
-//         } else {
-//             await knex("logins").insert({ username: username, password: password });
-//             // Redirect to the account page after successful user creation
-//             return res.redirect('/account');
-//         }
-//     }
-//     // Render the create page with the appropriate messages
-//     res.render('create', { errorMessage, loggedIn: req.session.loggedIn });
-// });
-
-// default rout to display landing page
+// default route to display landing page
 app.get("/", (req, res) => {
     res.render("index", {loggedIn : req.session.loggedIn});
 
 });
 
-// route to display landing page
-// app.get("/landingPage", (req, res) => {
-//     res.render("index", {loggedIn : req.session.loggedIn});
-// });
+// route to display create.ejs
+app.get("/create", checkLoggedIn, (req, res) => {
+    let errorMessage = null;
+    let successMessage = null;
+    let loggedInUsername = req.session.username;
 
-// route to send contact information from the form on the home (/landingPage) and info pages
-// app.post("/contact", (req, res) => {
-//     const { name, email, subject, message } = req.body;
+    if(loggedInUsername === "admin"){
+        res.render("create", {errorMessage, successMessage, loggedIn: req.session.loggedIn});
+    }
+    else{
+        res.render("login", {loggedIn: req.session.loggedIn});
+    }
+});
 
-//     // Process the form data as needed (e.g., send emails, save to a database)
+app.post('/create', async (req, res) => {
+    const { username, password, confirmPassword } = req.body;
+    let errorMessage = null;
 
-//     console.log('Form Data:', { name, email, subject, message });
+    // Check if the username already exists
+    const dbUser = await knex("logins").select().where('username', '=', username);
 
-//     res.send('Form submission successful!');
-// });
+    if (dbUser.length > 0) {
+        // Username already exists
+        errorMessage = 'That username is already being used';
+    } else {
+        // Continue with the user creation logic
+        if (password !== confirmPassword) {
+            errorMessage = 'Passwords need to match';
+        } else {
+            await knex("logins").insert({ username: username, password: password });
+            // Redirect to the account page after successful user creation
+            return res.redirect('/account');
+        }
+    }
+    // Render the create page with the appropriate messages
+    res.render('create', { errorMessage, loggedIn: req.session.loggedIn });
+});
 
 // route to display services.ejs
 app.get("/services", async (req,res) => {
@@ -134,17 +118,6 @@ app.get("/services", async (req,res) => {
         res.status(500).send('Internal Server Error');
     }
 });
-
-
-// route to dispaly survey
-// app.get("/survey", (req, res) => {
-//     res.render("survey", {loggedIn: req.session.loggedIn});
-// });
-
-// // route to display the tableau dashboard
-// app.get("/dashboard", (req, res) => {
-//     res.render("dashboard", {loggedIn: req.session.loggedIn});
-// });
 
 // route to display the login page
 app.get("/login", (req,res) => {
@@ -191,161 +164,92 @@ app.get("/logout", (req, res) => {
     })
 });
 
-// route to add a survey to our postrgres databases. Once submitted, it renders survey.ejs. Table names: "survey", "user", "user_platform", "user_organization"
-// app.post("/addRecord", async (req, res) => {
-//     // insert information to the "survey" table
-//     await knex("survey").insert({
-//         media_user: req.body.socialMediaUser,
-//         hours_on_media: req.body.hoursOnSocialMedia,
-//         use_media_no_purpose: req.body.useSocialMediaNoPurpose,
-//         distracted_by_media: req.body.distractedBySocialMediaRating,
-//         restless_not_using_media: req.body.restlessWhenNotUsingSocialMediaRating,
-//         easily_distracted: req.body.easilyDistractedGeneralRating,
-//         bothered_by_worries: req.body.botheredByWorriesGeneralRating,
-//         concentration: req.body.concentrationGeneralRating,
-//         compare_to_others: req.body.compareToOthersRating,
-//         how_comparisson_feels: req.body.howComparissonFeelsRating,
-//         seek_validation: req.body.seekValidationRating,
-//         depression: req.body.depressionGeneralRating,
-//         interest_fluctuate: req.body.interestFluctuateRating,
-//         general_sleep: req.body.generalSleepRating,
-//     });
-
-//     const survey_number = await knex("survey").select(knex.raw("cast(max(survey_number) as INT) as max_survey_number")).first(); // retrieve the most recent survey number
-//     const maxSurveyNumber = survey_number.max_survey_number; // set survey_number equal to the most recent survey number submitted. This variable will be used in the other three tables (other than the "survey" table where the survey number is auto generated
-//     const currentTimestamp = new Date();
-//     const targetTimezone = 'en-US';
-//     const formattedTimestamp = currentTimestamp.toLocaleString(targetTimezone, {
-//     timeZone: 'America/Denver', // Use 'America/Denver' for Mountain Time Zone
-//     month: 'numeric',
-//     day: '2-digit',
-//     year: '2-digit',
-//     hour: '2-digit',
-//     minute: '2-digit',
-//     second: '2-digit',
-//     })
-    
-//     // insert the survey information to the "user" table
-//     await knex("user").insert({
-//         survey_number: maxSurveyNumber,
-//         location: "Provo",
-//         timestamp: formattedTimestamp,
-//         age: req.body.age,
-//         gender: req.body.gender,
-//         relationship_status: req.body.relationshipStatus,
-//         occupation_status: req.body.occupation,    
-//     });
-
-//     // insert the survey information to the "user_platform" table to keep track of all the platforms a user selected as the ones they use
-//     let aPlatformName = req.body.platformName;
-//     for (const platform of aPlatformName) {
-//         if (platform != null) {
-//             await knex("user_platform").insert({
-//                 survey_number: maxSurveyNumber,
-//                 platform_number: platform
-//             });
-//         }
-//     }
-
-//     // insert the survey information to the "user_organization" table to keep track of all the organizations a user selects as being affiliated with
-//     let aOrganizationType = req.body.organizationType;
-//     for (const organization of aOrganizationType){
-//         if (organization != null){
-//             await knex("user_organization").insert({
-//                 survey_number: maxSurveyNumber,
-//                 organization_number: organization
-//             })
-//         }
-//     }
-//     res.render("survey", {loggedIn: req.session.loggedIn})
-// });
-
 // route to render account.ejs, but only when the user is logged in (using the checkLoggedIn function)
-// app.get("/account", checkLoggedIn, (req, res) => {
-//     tempusername = req.session.username
-//     if (tempusername === "admin") {
-//         // Display all data for the admin
-//         knex.select().from("logins").then(user => {
-//             res.render("account", { dbUser : user, loggedIn: req.session.loggedIn, loggedInUsername: req.session.username });
-//         });
-//     } else {
-//         // Display data only for the specific username
-//         knex.select().from("logins").where("username", tempusername).then(user => {
-//             res.render("account", { dbUser : user, loggedIn: req.session.loggedIn, loggedInUsername: req.session.username });
-//         });
-//     }
-// });
+app.get("/account", checkLoggedIn, (req, res) => {
+    tempusername = req.session.username
+    if (tempusername === "admin") {
+        // Display all data for the admin
+        knex.select().from("logins").then(user => {
+            res.render("account", { dbUser : user, loggedIn: req.session.loggedIn, loggedInUsername: req.session.username });
+        });
+    } else {
+        // Display data only for the specific username
+        knex.select().from("logins").where("username", tempusername).then(user => {
+            res.render("account", { dbUser : user, loggedIn: req.session.loggedIn, loggedInUsername: req.session.username });
+        });
+    }
+});
 
-// app.get("/edituser", checkLoggedIn, (req, res) => {
+app.get("/edituser", checkLoggedIn, (req, res) => {
 
-//     let currentUsername = req.query.editusername;
+    let currentUsername = req.query.editusername;
   
-//     // Fetch the current user's information
-//     knex.select().from("logins").where("username", "=", currentUsername)
-//       .then(user => {
-//         res.render("edituser", { currentUsername: currentUsername, currentUser: user[0], loggedIn: req.session.loggedIn, loggedInUsername: req.session.username });
-//     })
-//       .catch(error => {
-//         // Handle error
-//         console.error(error);
-//         res.status(500).send("Internal Server Error");
-//     });
-// });  
+    // Fetch the current user's information
+    knex.select().from("logins").where("username", "=", currentUsername)
+      .then(user => {
+        res.render("edituser", { currentUsername: currentUsername, currentUser: user[0], loggedIn: req.session.loggedIn, loggedInUsername: req.session.username });
+    })
+      .catch(error => {
+        // Handle error
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    });
+});  
 
 // update user username and password
-// app.post("/updateuser", (req, res) => {
-//     let currentUsername = req.body.currentUsername;
-//     let newUsername = req.body.newUsername;
-//     let newPassword = req.body.newPassword;
+app.post("/updateuser", (req, res) => {
+    let currentUsername = req.body.currentUsername;
+    let newUsername = req.body.newUsername;
+    let newPassword = req.body.newPassword;
   
-//     // Define an object to store the fields to be updated
-//     let updateFields = {};
+    // Define an object to store the fields to be updated
+    let updateFields = {};
   
-//     // Check if a new username is provided
-//     if (newUsername && newUsername.trim() !== "") {
-//       updateFields.username = newUsername;
-//     }
+    // Check if a new username is provided
+    if (newUsername && newUsername.trim() !== "") {
+      updateFields.username = newUsername;
+    }
   
-//     // Check if a new password is provided
-//     if (newPassword && newPassword.trim() !== "") {
-//       updateFields.password = newPassword;
-//     }
+    // Check if a new password is provided
+    if (newPassword && newPassword.trim() !== "") {
+      updateFields.password = newPassword;
+    }
   
-//     // Update the user's information in the database
-//     knex("logins")
-//       .where("username", "=", currentUsername)
-//       .update(updateFields)
-//       .then(() => {
-//         res.redirect("/account"); // Redirect to the home page or another appropriate location
-//       })
-//       .catch(error => {
-//         // Handle error
-//         console.error(error);
-//         res.status(500).send("Internal Server Error");
-//       });
-//   });   
+    // Update the user's information in the database
+    knex("logins")
+      .where("username", "=", currentUsername)
+      .update(updateFields)
+      .then(() => {
+        res.redirect("/account"); // Redirect to the home page or another appropriate location
+      })
+      .catch(error => {
+        // Handle error
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      });
+  });   
 
-//   app.post("/deleteuser", (req, res) => {
-//     let deleteUsername = req.body.deleteusername;
-//     let currentUsername = req.session.username;
+  app.post("/deleteuser", (req, res) => {
+    let deleteUsername = req.body.deleteusername;
+    let currentUsername = req.session.username;
 
-//     knex("logins")
-//         .where("username", "=", deleteUsername)
-//         .delete()
-//         .then(() => {
-//             // Check if the deleted account is the one logged in
-//             if (currentUsername === deleteUsername) {
-//                 res.redirect("/logout"); // Redirect to the login page
-//             } else {
-//                 res.redirect("account"); // Redirect to the home page or another appropriate location
-//             }
-//         })
-//         .catch(error => {
-//             // Handle error
-//             console.error(error);
-//             res.status(500).send("Internal Server Error");
-//         });
-//   });
+    knex("logins")
+        .where("username", "=", deleteUsername)
+        .delete()
+        .then(() => {
+            // Check if the deleted account is the one logged in
+            if (currentUsername === deleteUsername) {
+                res.redirect("/logout"); // Redirect to the login page
+            } else {
+                res.redirect("account"); // Redirect to the home page or another appropriate location
+            }
+        })
+        .catch(error => {
+            // Handle error
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        });
+  });
 
 // route to render data.ejs with a table of all the customer leads submitted
 app.get("/data", checkLoggedIn, async (req, res) => {
@@ -400,16 +304,6 @@ app.get("/datafiltered", async (req, res) => {
 app.get("/ourteam", (req,res) => {
     res.render('ourteam', {loggedIn: req.session.loggedIn})
 })
-
-// app.post("/addcustomer", async (req, res) => {
-//     // use req.query.interest to get the interest variable to insert
-
-//     await knex('customer').insert({
-       
-//     });
-
-//     res.redirect('/services')
-// })
 
 app.post("/addcustomer", async (req, res) => {
     const { firstname, lastname, email, phone } = req.body;
